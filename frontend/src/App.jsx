963c382494a494879ba71b19b02689413d05d657
Neemagrg <neemagurung1013@gmail.com>
2026-09-17 12:22:31 +0545

    Success

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,35 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
 import Home from "./pages/Home";
+import Tourism from "./pages/Tourism";
 
 function App() {
-  return <Home />;
+
+  return (
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/tourism"
+          element={<Tourism />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+
+  );
 }
 
 export default App;
